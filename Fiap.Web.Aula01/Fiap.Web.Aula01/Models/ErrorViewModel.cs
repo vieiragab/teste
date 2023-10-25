8ddc29ae08479114cc2d58bd191f662dfce320fc
Gabriella Vieira <90354488+vieiragab@users.noreply.github.com>
2023-10-25 20:24:52 -0300

    Delete Fiap.Web.Aula01 directory

--- Fiap.Web.Aula01/Fiap.Web.Aula01/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Fiap.Web.Aula01.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- Fiap.Web.Aula01/Fiap.Web.Aula01/Models/Produto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Fiap.Web.Aula01.Models
-{
-    public class Produto
-    {
-        public string Nome { get; set; }
-        public decimal Preco { get; set; }
-    }
-}
